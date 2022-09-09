7eef6bccd5e638a70003714a5f5a25ce5627ed61
avi4907 <112184444+avi4907@users.noreply.github.com>
2022-09-09 19:58:22 +0530

    strings

---
+++ string_basics.py
@@ -0,0 +1,51 @@
+my_string='hello'
+print(my_string)
+
+my_string="hello"
+print(my_string)
+
+my_string='''hello'''
+print(my_string)
+
+my_string="""hello"""
+print(my_string)
+
+print(my_string[0])
+print(my_string[-1])
+
+#Getting a Slice
+s='Encyclopedia'
+slice1=s[2:9]
+print(slice1)
+print(slice1[-1::])
+
+x=chr(65)
+print(x)
+
+x=chr(2365)
+print(x)
+
+x=chr(12365)
+print(x)
+
+for i in range(200,15000):
+    print(i,chr(i))
+
+y=ord('A')
+print(y)
+
+y=ord('a')
+print(y)
+
+y=ord('{')
+print(y)
+
+w1='this'
+w2='is'
+w3='Amazing'
+msg= w1 + ' '+w2 +' '+ w3
+print(msg)
+
+#duplication
+
+print('_'*100)
